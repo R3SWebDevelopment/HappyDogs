15d6d5f72c9613ec5875b7fa60f2c279a7017421
Ricardo Tercero Solis <ricardo.tercero.solis@gmail.com>
2016-12-26 11:24:12 -0600

    Model has been created Basic functionality of the model created
    Without any testing perform
    
    Some funtionality need to be added to valid the date input

---
+++ HappyDogs/HappyDogs/apps/HappyDogs/models.py
@@ -0,0 +1,131 @@
+from django.db import models
+from shortuuidfield import ShortUUIDField
+from datetime import date
+import datetime
+INPUT_DATE_FORMAT = '%m/%d/%Y'
+
+class Dog(models.Model):
+    uuid = ShortUUIDField(max_length=255, db_index=False)
+    first_name = models.TextField(null=False, blank=False)
+    last_name = models.TextField(null=False, blank=False, default ="")
+    full_name = models.TextField(null=False, blank=False, unique=True)
+
+    class Meta:
+        ordering = ['full_name']
+
+    @classmethod
+    def add(cls, first_name=None, last_name=None, fetching=False):
+        created = False
+        instance = None
+        if first_name is not None and first_name.__class__ is str and first_name.strip():
+            full_name = "{} {}".format(first_name , last_name or "").strip().upper()
+            instance, created = cls.objects.got_or_create(full_name = full_name)
+            if instance is not None and created:
+                instance.first_name = first_name
+                instance.last_name = last_name
+                instance.save()
+            elif instance is not None and not created and not fetching:
+                raise Exception('The Dog already Exists on the System')
+            elif instance is None and not fetching:
+                raise Exception('The Dog has not beed registred.')
+        elif not fetching:
+            raise Exception('The Dog Should Have at least First Name')
+        return instance, created
+
+    @classmethod
+    def is_dog_name_registred(cls, first_name=None, last_name=None):
+        if first_name is not None and first_name.__class__ is str and first_name.strip():
+            full_name = "{} {}".format(first_name , last_name or "").strip().upper()
+            return cls.objects.filter(full_name__iexact = full_name).exists()
+        return False
+
+    def is_the_house(self, date=None):
+        visits = self.boarding_visits
+        if visits is not None:
+            if date is None:
+                date = date.today()
+            filtered_visits = visits.filter(start_date__lte = date)
+            if filtered_visits.exists():
+                filtered_visits = filtered_visits.filter(end_date__gt = date)
+                return filtered_visits.exists()
+        return False
+
+    def add_visit(self, start_date=None, end_date=None):
+        visit = None
+        created = False
+        if start_date is None:
+            raise Exception('Start Date is a Required Field')
+        elif start_date.__class__ is not Date and start_date.__class__ is str:
+            try:
+                datetime.datetime.strptime(start_date, INPUT_DATE_FORMAT)
+            except:
+                raise Exception('Start Date Field is Wrong Format')
+        elif start_date.__class__ is not Date:
+            raise Exception('Start Date Field is Wrong Format')
+        if end_date is None:
+            raise Exception('End Date is a Required Field')
+        elif end_date.__class__ is not Date and end_date.__class__ is str:
+            try:
+                datetime.datetime.strptime(end_date, INPUT_DATE_FORMAT)
+            except:
+                raise Exception('End Date Field is Wrong Format')
+        elif end_date.__class__ is not Date:
+            raise Exception('End Date Field is Wrong Format')
+        visit, created = BoardingVisit.add(dog=self, start_date=start_date, end_date=end_date)
+        return visit, created
+
+    @classmethod
+    def add_dog_visit(cls, dog_first_name=None, dog_last_name=None, start_date=None, end_date=None):
+        dog, dog_created = cls.add(first_name=dog_first_name, last_name=dog_last_name, fetching=True)
+        visit = None
+        if dog is not None:
+            visit = dog.add_visit(start_date=start_date, end_date=end_date)
+        return dog, visit
+
+###TODO
+######ADD SAVE METHOD OVERRIDER TO VALID THAT THE FULL NAME IS NOT DUPLICATED
+
+class BoardingVisit(models.Model):
+    dog = models.ForeignKey('Dog', blank=False, null=False, related_name='boarding_visits')
+    start_date = models.DateField(blank=False, null=False)
+    end_date = models.DateField(blank=False, null=False)
+
+    class Meta:
+        ordering = ['start_date', 'end_date', 'dog']
+
+    @classmethod
+    def dog_has_visit(cls, dog=None):
+        if dog is not None and dog.__class__ is Dog:
+            return cls.objects.filter(dog=dog).exists()
+        return False
+
+    @classmethod
+    def dog_visits(cls, dog=None):
+        data = cls.objects.none()
+        if dog is not None and dog.__class__ is Dog:
+            data = cls.objects.filter(dog=dog)
+        return data
+
+    @classmethod
+    def dog_has_overlaping_dates(cls, dog=None, start_date=None, end_date=None):
+        if dog is not None and dog.__class__ is Dog and start_date is not None and start_date.__class__ is date and end_date is not None and end_date.__class__ is date:
+            if cls.dog_has_visit(dog=dog) and start_date < end_date:
+                visits = cls.dog_visits(dog=dog)
+                if visits is not None:
+                    #######HERE COMES THE DATE RANGE FILTERING
+                    return True
+        return False
+
+    @classmethod
+    def add(cls, dog=None, start_date=None, end_date=None):
+        instance = None
+        created = False
+        return instance, created
+
+    @classmethod
+    def clear(cls):
+        cls.object.all().delete()
+
+    @classmethod
+    def randomized_boarding(cls):
+        cls.clear()
