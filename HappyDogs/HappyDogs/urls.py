b013a84227adf2f25c09519762c2610ea8a500da
Ricardo Tercero Solis <ricardo.tercero.solis@gmail.com>
2016-12-26 23:34:36 -0600

    Test Unit

--- HappyDogs/HappyDogs/urls.py
+++ HappyDogs/HappyDogs/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import include, url
+from HappyDogs.apps.HappyDogs.views import happy_dogs_home
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
+    url(r'^$', happy_dogs_home , name='index'),
     url(r'^happy_dogs/', include('HappyDogs.apps.HappyDogs.urls')),
     url(r'^rest/happy_dogs/', include('HappyDogs.apps.HappyDogs.rest_urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
